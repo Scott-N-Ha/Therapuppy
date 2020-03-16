ff23f848c6ce80e4817509572adba325c145c289
anson <ansonchongwus44@gmail.com>
2020-03-15 21:54:08 -0700

    Add pupppy

---
+++ models/Puppy.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PuppySchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    renter: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    name: {
+        type: String, 
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    fluffyRating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    earTypeId: {
+        type: Schema.Types.ObjectId,
+        ref: "earTypes"
+    },
+    sex: {
+        type: String,
+        enum: ['M','F'],
+        required: true
+    },
+    natureRating: {
+        type: Number,
+        min: 1,
+        max: 10,
+        required: true
+    },
+    price: {
+        type: Number, 
+        required: true
+    }
+});
+
+const Puppy = mongoose.model('puppies', PuppySchema);
+module.exports = Puppy; 
