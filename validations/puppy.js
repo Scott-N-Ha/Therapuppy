832fa35f56573e83062f5fe98da2c1378cde7b3d
anson <ansonchongwus44@gmail.com>
2020-03-15 22:56:45 -0700

    Add puppy validations

---
+++ validations/puppy.js
@@ -0,0 +1,11 @@
+const Validator = require('validator');
+const validText = require("./valid_text");
+
+module.exports = function validatePuppyInput(data) {
+    let errors = {};
+
+    data.name = validText(data.name) ? data.name : "";
+    data.sex = validText(data.sex) ? data.sex : "";
+    
+
+};
