c8029692183d1920a86c52a7a97c4183f7c0cd4c
Scott Ha <123viet123@gmail.com>
2020-03-17 10:16:41 -0700

    Cleaned up comments

--- app.js
+++ app.js
@@ -8,48 +8,12 @@ const validateRegisterInput = require("./validations/register");
 const validatePuppyInput = require("./validations/puppy");
 const User = require('./models/User');
 
-// const users = require("./routes/api/users.js");
-// const tweets = require("./routes/api/tweets.js");
-// const User = require("./models/User.js");
-
 const app = express();
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('frontend/build'));
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'src', 'index.js'));
-//   });
-// }
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB."))
   .catch(err => console.log(err));
-
-// const u1 = {
-//   username: "abc2",
-//   email: "asdasd@aa.io",
-//   firstName: "aabc",
-//   lastName: "bcde",
-//   password: "password",
-//   password2: "password",
-//   isOwner: true,
-//   address1: "1236 jackson street",
-//   city: "San Francisco",
-//   state: "WA",
-//   zip: "98004"
-// };
-
-
-// const validator = validatePuppyInput(u1); 
-
-// if (validator.isValid){
-//   console.log("valid")
-//   console.log(u1);
-//   // User.create(u1);
-// }
-// else {
-//   console.log(validator.errors);
-// }
   
 app.use(passport.initialize());
 require('./config/passport.js')(passport);
@@ -60,9 +24,6 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-// // app.use("/api/users", users);
-// // app.use("/api/tweets", tweets);
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
@@ -71,5 +32,4 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   res.send("its running")
-  // res.send(path.resolve(__dirname, 'frontend', 'src', 'index.jsx'));
 });
