4d44f71f060705d3d78fa369138eb6e7a75bf707
olivia <yoon.eunsil@gmail.com>
2020-03-17 14:39:37 -0700

    Debug how data is being passed into reducers

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,4 +1,4 @@
-import {  } from '../actions/session_actions.js';
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions.js';
 
 const initialState = {
 
@@ -7,8 +7,11 @@ const initialState = {
 const usersReducer = (state = initialState, action) => {
   Object.freeze(state);
   let nextState = Object.assign({}, state);
-
   switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+
+      nextState[action.currentUser._id] = action.currentUser
+      return nextState
     default:
       return state;
   }
