8e76c3bae1e2cc54c42b651ddc4cab75fe757320
Scott Ha <123viet123@gmail.com>
2020-03-17 15:32:59 -0700

    Beginning work on MainPageContainer

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PuppyIndexContainer from '../puppy/puppy_index_container.js';
+
 class MainPage extends React.Component {
   constructor(props){
     super(props);
@@ -17,9 +19,12 @@ class MainPage extends React.Component {
         </div>
       )
     } else {
+      debugger
       return (
         <div>
           Main Page when User is Logged In
+          There's a puppy index container right here but we have no puppies.
+          <PuppyIndexContainer />
         </div>
       )
     }
