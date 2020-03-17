76f33ce35a0319938d36f8b136ab88433a3a14d0
mckgegis <mckgegis@yahoo.com>
2020-03-16 17:28:50 -0700

    update booking validations

--- validations/booking.js
+++ validations/booking.js
@@ -7,45 +7,56 @@ module.exports = function validateLoginInput(data) {
         errors.ownerId = 'Owner Id field is required'
     };
 
-    if (Number.isInteger(data.ownerId)) {
-        errors.ownerId = 'Owener Id field must be an integer'
-    };
-
     if (Validator.isEmpty(data.renterId)) {
         errors.renterId = 'Renter Id field is required'
-    };
-
-    if (Number.isInteger(data.renterId)) {
-        errors.renterId = 'Renter Id field must be an integer'
     };
 
     if (Validator.isEmpty(data.puppyId)) {
         errors.puppyId = 'Puppy Id field is required'
     };
-
-    if (Number.isInteger(data.puppyId)) {
-        errors.puppyId = 'Puppy Id field must be an integer'
+    
+    if (!Validator.isISO8601(data.date)) {
+        errors.date = 'Date field must be in date format'
     };
 
     if (Validator.isEmpty(data.date)) {
         errors.date = 'Date field is required'
     };
 
+    if (!Validator.isISO8601(data.timeStart)) {
+        errors.timeStart = 'Start time must be a time format'
+    };
+
     if (Validator.isEmpty(data.timeStart)) {
         errors.timeStart = 'Start time field is required'
+    };
+
+    if (!Validator.isFloat(data.duration)) {
+        errors.duration = 'Duration field must be a float'
     };
 
     if (Validator.isEmpty(data.duration)) {
         errors.duration = 'Duration field is required'
     };
 
+    if (Validator.isISO8601(data.timeEnd)) {
+        errors.timeEnd = 'End time field must be in time format'
+    };
+
+    if (Validator.isEmpty(data.timeEnd)) {
+        errors.timeEnd = 'End time field is required'
+    };
+
     if (Validator.isEmpty(data.statusId)) {
         errors.statusId = 'Status field is required'
+    };
+
+    if (Validator.isFloat(data.totalCost)) {
+        errors.totalCost = 'Total cost field must be a float'
     };
 
     if (Validator.isEmpty(data.totalCost)) {
         errors.totalCost = 'Total cost field is required'
     };
-
 
 }
