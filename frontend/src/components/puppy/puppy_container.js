f250c0894d625cb446fc5c323913213e47d807bc
Scott Ha <123viet123@gmail.com>
2020-03-18 11:20:25 -0700

    Fixed puppy.jsx

--- frontend/src/components/puppy/puppy_container.js
+++ frontend/src/components/puppy/puppy_container.js
@@ -4,9 +4,10 @@ import { withRouter } from 'react-router-dom';
 import Puppy from './puppy.jsx';
 
 const mapStateToProps = (state, { puppy }) => {
+  debugger
   return {
     owner: state.entities.users[puppy.owner],
-    breed: state.entities.breeds[puppy.breed],
+    breed: state.entities.breeds[puppy.breed].name,
     fluffyRating: state.entities.fluffyRatings[puppy.fluffyRating].name,
     natureRating: state.entities.natureRatings[puppy.natureRating].name,
   };
