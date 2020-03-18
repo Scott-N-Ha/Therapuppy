ac06bd888578f04f43404b6b4a76c89b088ea6f2
olivia <yoon.eunsil@gmail.com>
2020-03-18 11:36:14 -0700

    Add msp and mdp to puppy show container

--- frontend/src/components/puppy/puppy_show.jsx
+++ frontend/src/components/puppy/puppy_show.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 import BookingIndexContainer from '../booking/booking_index_container.js';
 
 export default class PuppyShow extends React.Component {
@@ -18,13 +18,31 @@ export default class PuppyShow extends React.Component {
   }
 
   render(){
-    const { puppy } = this.props;
+    const { puppy, owner, breed, fluffyRating, natureRating, earType } = this.props;
+    const { name, age, sex, price } = puppy;
 
     if (puppy === undefined) return this.missingPuppy();
 
     return (
       <div className="puppy-show">
         Puppy information goes here
+        <label className="puppy-owner"><Link to={`/users/${owner.username}`}>{owner.firstName} {owner.lastName}</Link></label>
+        <br/>
+        <label className="puppy-name"><Link to={`/puppies/${puppy._id}`}>{name}</Link></label>
+        <br/>
+        <label className="puppy-age">{age}</label>
+        <br/>
+        <label className="puppy-breed">{breed}</label>
+        <br/>
+        <label className="puppy-fluffyRating">{fluffyRating}</label>
+        <br/>
+        <label className="puppy-earType">{earType}</label>
+        <br/>
+        <label className="puppy-sex">{sex}</label>
+        <br/>
+        <label className="puppy-natureRating">{natureRating}</label>
+        <br/>
+        <label className="puppy-price">${price}</label>
 
         <BookingIndexContainer puppyId={puppy._id} />
       </div>
--- frontend/src/components/puppy/puppy_show_container.js
+++ frontend/src/components/puppy/puppy_show_container.js
@@ -5,9 +5,14 @@ import PuppyShow from './puppy_show.jsx';
 
 const mapStateToProps = (state, ownProps) => {
   let puppy = state.entities.puppies[ownProps.match.params.puppyId];
-  
+  debugger
   return {
     puppy: puppy,
+    owner: state.entities.users[puppy.owner],
+    breed: state.entities.breeds[puppy.breed].name,
+    fluffyRating: state.entities.fluffyRatings[puppy.fluffyRating].name,
+    natureRating: state.entities.natureRatings[puppy.natureRating].name,
+    earType: state.entities.earTypes[puppy.earType].name
   }
 };
 
