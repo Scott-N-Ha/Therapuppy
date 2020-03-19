3d3472194dcaf305fcb68485f72779648d530e1c
Scott Ha <123viet123@gmail.com>
2020-03-19 13:24:52 -0700

    Added binding for renders

--- frontend/src/components/puppy/puppy.jsx
+++ frontend/src/components/puppy/puppy.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 export default class Puppy extends React.Component {
   constructor(props){
     super(props);
+
+    this.renderMore = this.renderMore.bind(this);
+    this.renderLess = this.renderLess.bind(this);
   }
 
   renderMore(){
