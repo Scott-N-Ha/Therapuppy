59c61efca2e270a2b1113f10878903c62dd77997
Scott Ha <123viet123@gmail.com>
2020-03-19 11:33:28 -0700

    Booking Route Error

--- routes/api/bookings.js
+++ routes/api/bookings.js
@@ -45,8 +45,8 @@ router.post("/",
 
               User.findById(owner)
                 .then(owner => {
-                  owner.bookings.push(booking.id)
-                  owner.save()
+                  owner.bookings.push(booking.id);
+                  owner.save();
                   User.findById(renter)
                     .then(renter => {
                       renter.bookings.push(booking.id)
@@ -63,8 +63,8 @@ router.post("/",
             })
             .catch(err => console.log(err))
         } else {
-          return res.status(400).json({
-            puppynotfound: "cannot find puppp"
+          return res.status(404).json({
+            puppynotfound: "cannot find puppy",
           });
         }
       })
