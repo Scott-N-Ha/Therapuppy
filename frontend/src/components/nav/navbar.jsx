b41dbd507e65db07c22a5ed975e80463431b3ebb
olivia <yoon.eunsil@gmail.com>
2020-03-18 20:31:26 -0700

    add styling for signed in header

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -23,34 +23,30 @@ class NavBar extends React.Component {
     const {openModal} = this.props
     if (this.props.loggedIn){
       return (
+        <div className="nav-bar logged">
+          <div className="nav-bar-logo-container">
+        <Link to="/"><h1 className="therapuppy-header">THERAP(UPP)Y</h1></Link>
+        </div>
         <div className="logout-button">
           <Link to='/puppies'>Certified Dogters</Link>
           <button onClick={this.logoutUser}>Logout</button>
         </div>
+        </div>
       )
     } else {
-      return (
-        // <div className="login-buttons">
-        //   <button
-        //     onClick={() => this.handleModal("login")}
-        //     className="navbar-btn">Login</button>
-        //   <button
-        //     onClick={() => this.handleModal("signup")}
-        //     className="navbar-btn">Wagister</button>
-        // </div>
-        null
-      )
-    }
+      return (<div className="nav-bar">
+        <div className="nav-bar-logo-container">
+        <Link to="/"><h1 className="therapuppy-header">THERAP(UPP)Y</h1></Link>
+        </div>
+      </div>)
+    } 
   }
 
   render(){
     return (
-      <div className="nav-bar">
-        <div className="nav-bar-logo-container">
-        <Link to="/"><h1 className="therapuppy-header">THERAP(UPP)Y</h1></Link>
-        </div>
-        { this.getLinks() }
-      </div>
+      <>
+        {this.getLinks()} 
+      </>
     )
   }
 }
