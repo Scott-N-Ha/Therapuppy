c60c4a8d2eee12d8a38e6047603833d4a7b65d52
olivia <yoon.eunsil@gmail.com>
2020-03-22 02:36:41 -0700

    Remove debugger

--- frontend/src/components/puppy/puppy.jsx
+++ frontend/src/components/puppy/puppy.jsx
@@ -87,7 +87,7 @@ export default class Puppy extends React.Component {
         </div>
         </div>)}
       </div>
-      {/* <BookingIndexContainer puppyId={puppy._id} /> */}
+      <BookingIndexContainer puppyId={puppy._id} />
       </>
     )
   }
--- frontend/src/util/booking_api_util.js
+++ frontend/src/util/booking_api_util.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetchBookings = () => {
-  debugger
+  // debugger
   return(
   axios.get('api/bookings')
 );}
