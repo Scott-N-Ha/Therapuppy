1d35bcd12ca29b876a081f34a1afc19166a3a5dd
anson <ansonchongwus44@gmail.com>
2020-03-22 17:31:36 -0700

    test

--- frontend/src/components/puppy/puppy.jsx
+++ frontend/src/components/puppy/puppy.jsx
@@ -81,6 +81,7 @@ export default class Puppy extends React.Component {
           <button className="request-dogter-button" onClick={() => openModal("requestDogter")}> 
           Request Dogter
           </button>
+          
           <Link to={`/users/${owner.username}`} className="request-dogter-button owner" > 
           Owner Information
           </Link>
